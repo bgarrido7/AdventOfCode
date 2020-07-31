b37b9501a7ed2a0301351fb46b2723915fd12712
bgarrido7 <bianevesgarrido@gmail.com>
2020-07-31 16:18:28 +0100

    lvl1 done mas estou a tentar arranjar um prob de variaveis

--- lvl1/01.js
+++ lvl1/01.js
@@ -1,2 +1,28 @@
+/*
+fs = require('fs')
+fs.readFile('./input.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data); //imprime o file todo
+});
+*/
 
-console.log("ah")
+
+
+//-------------isto sao cenas do node---------------
+var freq = 0
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('input.txt')
+  });
+  
+//-------------ler linha a linha---------------
+  lineReader.on('line', function (line) {
+    freq += parseInt(line)
+    console.log(freq) //soluçao é o ultimo print desde log
+  });
+
+
+//-------------------------------------------------
+
+console.log(freq) //nao faço ideia pqq esta variabel nao é a mesma que está dentro da funçao >:(
