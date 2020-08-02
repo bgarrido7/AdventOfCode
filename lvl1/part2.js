946f89597948e3e838dd3e092d87c548c6fdf474
bgarrido7 <bianevesgarrido@gmail.com>
2020-08-02 17:46:14 +0100

    demorou bue a correr mas deu :)

--- lvl1/part2.js
+++ lvl1/part2.js
@@ -1,15 +1,18 @@
-const line = require('fs').readFileSync('input.txt').toString().split('\n');
+const line = require('fs').readFileSync('example2.txt').toString().split('\n');
 
 function sum(){
     let freq = 0;
     let found = false;
     let compare = [0]
+    
     while(!found){
         for (let key in line){
             if( !isNaN( parseInt(line[key]) ) ){
                 freq += parseInt(line[key]);
                 //console.log(compare)
+
                 if(compare.some(elem => elem == freq)){
+                    console.log("found it!")
                     found = true
                     return freq
                 }
