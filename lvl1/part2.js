6b08aba2994aa6d13b21d1e4a100809927fa7084
bgarrido7 <bianevesgarrido@gmail.com>
2020-08-02 17:38:19 +0100

    i think its done part 2 but result takes a long time to compute

--- lvl1/part2.js
+++ lvl1/part2.js
@@ -1,17 +1,22 @@
 const line = require('fs').readFileSync('input.txt').toString().split('\n');
 
-
 function sum(){
-    var freq = 0;
-
-    for (let key in line){
-        if( !isNaN( parseInt(line[key]) ) ){
-            //console.log(parseInt(line[key]))
-            freq += parseInt(line[key]);
-            
+    let freq = 0;
+    let found = false;
+    let compare = [0]
+    while(!found){
+        for (let key in line){
+            if( !isNaN( parseInt(line[key]) ) ){
+                freq += parseInt(line[key]);
+                //console.log(compare)
+                if(compare.some(elem => elem == freq)){
+                    found = true
+                    return freq
+                }
+                compare.push(freq)
+            }
         }
     }
-    return freq;
 }
 
 console.log(sum())
