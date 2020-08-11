fbdd9ac3279a5fa2f3406de3797481e48e6123d0
bgarrido7 <bianevesgarrido@gmail.com>
2020-08-11 17:01:16 +0100

    part 1 done mas podia estar melhor o codigo

--- lvl2/01.js
+++ lvl2/01.js
@@ -1,4 +1,4 @@
-const line = require('fs').readFileSync('./example.txt').toString().split('\n');
+const line = require('fs').readFileSync('./input.txt').toString().split('\n');
 
 let twice = 0
 let three = 0
@@ -6,20 +6,20 @@ function checksum(){
 
     line.forEach( word => {
         let letter=[]
-        console.log(word)
+        //console.log(word)
 
         for(let l in word){
             letter.push(word[l])            
         }
         letter.sort()
-        let i=0
 
+        let i=0
         while(i<letter.length){
             if(letter[i]==letter[i+1]){
                 if(letter[i]==letter[i+2])
                     i+=3
                 else{
-                    console.log('contains a letter which appears exactly twice!')
+                    //console.log('contains a letter which appears exactly twice!')
                     twice++
                     break
                 }
@@ -31,41 +31,15 @@ function checksum(){
         let j=0
         while(j<letter.length){
             if(letter[j]==letter[j+1] && letter[j]==letter[j+2]){
-                console.log('contains a letter which appears exactly 3 times!')
+                //console.log('contains a letter which appears exactly 3 times!')
                 three++
                 break               
             }
             else
                 j++
         }
-
- /*      
-        letter.forEach( (elem, index, array) => {
-
-            if(array[index]==array[index+1]){
-
-console.log(elem)
-
-                if(array[index]==array[index+2]){
-                    console.log('found three!')
-                    three++
-                    return                 
-            }
-            else{
-                console.log('found two!')
-                twice++
-                
-            }
-            return
-            }
-        })
-*/
-
-        console.log('------\n')
-
     })
     console.log(twice+ ' * ' + three +' = '+ twice*three)
-    
 }
 
 checksum()
