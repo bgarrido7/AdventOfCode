0c3a834177549540577262f35129fff18f7ba2b6
bgarrido7 <bianevesgarrido@gmail.com>
2020-08-12 19:30:27 +0100

    code cleaned :sunglasses:

--- lvl3/02.js
+++ lvl3/02.js
@@ -2,49 +2,15 @@
 const line = require('fs').readFileSync('./input.txt').toString().split('\n');
 
 
-function checkID(){
-
-    let fabric = [];
+function updateFabric(input, fabric, status){
     let res = 0
-    for (var i = 0; i < 1000; i++) {//fill in the multidimention array
-        fabric[i] = [];
-        for (var j = 0; j < 1000; j++) {
-            fabric[i][j] = ".";
-        }
-    }
 
-    for (let i in line){
+    for (let i in input){
 
-        let id = (line[i].split('#')[1].split(' ')[0])
+        let id = (input[i].split('#')[1].split(' ')[0])
 
-        let pos = line[i].split('@')[1].split(':')[0]
-        let size = line[i].split('@')[1].split(':')[1]
-
-        let x = parseInt(pos.split(',')[0])
-        let y = parseInt(pos.split(',')[1])
-
-        let width = parseInt(size.split('x')[0])
-        let height = parseInt(size.split('x')[1])
-
-        for(let i = x; i<(x+width);i++){ // fill in the array according to the input given
-            for(let j = y;j<(y+height);j++){
-
-                if(fabric[j][i] == '.'){
-                    fabric[j][i] = id
-                }        
-                else{
-                    fabric[j][i] = 'X'
-                }
-            }  
-        }
-    }
-//console.log(fabric)
-    for (let i in line){
-
-        let id = (line[i].split('#')[1].split(' ')[0])
-
-        let pos = line[i].split('@')[1].split(':')[0]
-        let size = line[i].split('@')[1].split(':')[1]
+        let pos = input[i].split('@')[1].split(':')[0]
+        let size = input[i].split('@')[1].split(':')[1]
 
         let x = parseInt(pos.split(',')[0])
         let y = parseInt(pos.split(',')[1])
@@ -54,17 +20,50 @@ function checkID(){
 
         let unique = true
 
-        for(let i = x; i<(x+width);i++){ // iterate the array again
+        for(let i = x; i<(x+width);i++){
             for(let j = y;j<(y+height);j++){
 
-                if(fabric[j][i] == 'X')
-                    unique = false
-                       
+                switch(status){
+                    case 0: // fill in the array according to the input given
+                        
+                        if(fabric[j][i] == '.'){ fabric[j][i] = id }
+                        else { fabric[j][i] = 'X' }
+                                    
+                    case 1: // iterate the array again, check for overlapping id's
 
-            }  
+                        if(fabric[j][i] == 'X') { unique = false }
+                                   
+                            
+                }
+            }   
         }
-        if(unique){ res = id ; console.log(res)} 
+
+        if(unique){ res = id } 
     }
+    
+
+    if(status)
+        return res 
+    else 
+        return fabric 
+    
+}
+
+
+function checkID(){
+
+    let fabric = [];
+    for (var i = 0; i < 1000; i++) {//fill in the multidimention array
+        fabric[i] = [];
+        for (var j = 0; j < 1000; j++) {
+            fabric[i][j] = ".";
+        }
+    }
+
+    let upadtedFarbic = updateFabric(line, fabric, 0)
+    let awnser =  updateFabric(line, upadtedFarbic, 1)
+
+    console.log(awnser)
 }
 
 checkID()
