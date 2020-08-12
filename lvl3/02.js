3258fc8c17ed54e20f6d84003ef4c889321161eb
bgarrido7 <bianevesgarrido@gmail.com>
2020-08-12 18:33:24 +0100

    part2 of lvl3 done :sunglasses: missing code clean tho

--- lvl3/02.js
+++ lvl3/02.js
@@ -1,19 +1,19 @@
-const line = require('fs').readFileSync('./example.txt').toString().split('\n');
-//const line = require('fs').readFileSync('./input.txt').toString().split('\n');
+//const line = require('fs').readFileSync('./example.txt').toString().split('\n');
+const line = require('fs').readFileSync('./input.txt').toString().split('\n');
+
 
 function checkID(){
+
     let fabric = [];
     let res = 0
-
-    for (var i = 0; i < 10; i++) {//fill in the multidimention array
+    for (var i = 0; i < 1000; i++) {//fill in the multidimention array
         fabric[i] = [];
-        for (var j = 0; j < 10; j++) {
+        for (var j = 0; j < 1000; j++) {
             fabric[i][j] = ".";
         }
     }
 
     for (let i in line){
-        let unique = true
 
         let id = (line[i].split('#')[1].split(' ')[0])
 
@@ -34,25 +34,37 @@ function checkID(){
                 }        
                 else{
                     fabric[j][i] = 'X'
-                    unique=false
                 }
             }  
         }
-        if(unique){ res = id } 
     }
-    //console.log(fabric)
-/*
-    fabric.forEach(row => {
-        console.log(row)
-        row.forEach(cell => {
-            if(cell=='X'){
-                unique = true
-            }
-        })
-    })
-*/
-    console.log(res)
+//console.log(fabric)
+    for (let i in line){
 
+        let id = (line[i].split('#')[1].split(' ')[0])
+
+        let pos = line[i].split('@')[1].split(':')[0]
+        let size = line[i].split('@')[1].split(':')[1]
+
+        let x = parseInt(pos.split(',')[0])
+        let y = parseInt(pos.split(',')[1])
+
+        let width = parseInt(size.split('x')[0])
+        let height = parseInt(size.split('x')[1])
+
+        let unique = true
+
+        for(let i = x; i<(x+width);i++){ // iterate the array again
+            for(let j = y;j<(y+height);j++){
+
+                if(fabric[j][i] == 'X')
+                    unique = false
+                       
+
+            }  
+        }
+        if(unique){ res = id ; console.log(res)} 
+    }
 }
 
 checkID()
