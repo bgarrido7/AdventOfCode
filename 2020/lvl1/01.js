c3c1f57de8a36d73831250fa97d691c72b711a8c
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-01 12:32:20 +0000

    day1 part1 done

--- 2020/lvl1/01.js
+++ 2020/lvl1/01.js
@@ -1,1 +1,19 @@
-console.log('hi')
+
+//const line = require('fs').readFileSync('example.txt').toString().split('\n');
+const line = require('fs').readFileSync('input.txt').toString().split('\n');
+
+
+function doThing(){
+    let num1, num2 = 0;
+    line.forEach((elem, index, vector)=>{
+        vector.forEach(e =>{
+            if( (parseInt(e) + parseInt(elem) ) == 2020){
+                num1 = parseInt(e);
+                num2 = parseInt(elem);
+            }
+        });
+    });
+    return (num1 * num2);
+}
+
+console.log(doThing());
