4da5d43a059dfee7455ef2821982d61bbb34afec
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-02 15:18:17 +0000

    day 2 done :happy:

--- 2020/lvl2/02.js
+++ 2020/lvl2/02.js
@@ -1,11 +1,25 @@
 
-const line = require('fs').readFileSync('example.txt').toString().split('\n');
-//const line = require('fs').readFileSync('input.txt').toString().split('\n');
+//const line = require('fs').readFileSync('example.txt').toString().split('\n');
+const line = require('fs').readFileSync('input.txt').toString().split('\n');
 
 
 function doThing(){
-   
-    return ;
+    let valid = 0;
+    line.forEach(l=>{
+        let policy = l.split(':')[0];
+        let pass = l.split(':')[1]; //index 0 is a ' ' so its like if the string starts at index 1
+
+        let letter = policy.split(' ')[1];
+
+        let char1 = pass.charAt(parseInt(policy.split('-')[0]));
+        let char2 = pass.charAt(parseInt(policy.split('-')[1].split(' ')[0]));
+
+        if((char1===letter && char2!==letter) || (char2===letter && char1!==letter) )
+            valid++;
+
+    });
+
+    return valid;
 }
 
 console.log(doThing());
