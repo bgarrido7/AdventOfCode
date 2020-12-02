210bff91b6cbf88a2d55e7142ee2cb8e05a9889a
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-02 14:41:58 +0000

    part1 day 2 done

---
+++ 2020/lvl2/01.js
@@ -0,0 +1,30 @@
+
+//const line = require('fs').readFileSync('example.txt').toString().split('\n');
+const line = require('fs').readFileSync('input.txt').toString().split('\n');
+
+
+function doThing(){
+    let valid = 0;
+    line.forEach(l=>{
+        let policy = l.split(':')[0];
+        let pass = l.split(':')[1];
+
+        let letter = policy.split(' ')[1];
+        let min = policy.split('-')[0];
+        let max = policy.split('-')[1].split(' ')[0];
+
+        let count = 0;
+
+        for(let el in pass){
+            if(pass[el]===letter)
+                count++;
+        }
+
+        if(count >= min && count <= max)
+            valid++;
+    });
+
+    return valid;
+}
+
+console.log(doThing());
---
+++ 2020/lvl2/02.js
@@ -0,0 +1,11 @@
+
+const line = require('fs').readFileSync('example.txt').toString().split('\n');
+//const line = require('fs').readFileSync('input.txt').toString().split('\n');
+
+
+function doThing(){
+   
+    return ;
+}
+
+console.log(doThing());
