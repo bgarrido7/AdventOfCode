3feb4cfb22d061fdb65b35b8b053e57ad980c155
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-03 14:05:32 +0000

    day 2 part 2 mistake

--- 2020/lvl3/02.js
+++ 2020/lvl3/02.js
@@ -8,17 +8,45 @@ function doThing() {
     pattern.forEach(function() { count++ });
     let xTimes = (count - 1) * 2;
 
-    let x = -3;
-    pattern.forEach(elem => {
-        x += 3;
-        if (x == 0) { return; } //skips the first line
-        else {
-            let p = elem.repeat(xTimes);
-            if (p.charAt(x) === "#")
-                trees++;
-        }
-    })
+    let checkX = [1, 3, 5, 7, 1];
+    let checkY = [1, 1, 1, 1, 2];
 
+    checkX.forEach((c, index, vector) => {
+        let x = -c;
+        //console.log("Y number:", checkY[index])
+        if (checkY[index] === 1) {
+            let t = 0;
+            pattern.forEach((elem) => {
+                x += c;
+                if (x == 0) { return; } //skips the first line
+                else {
+                    let p = elem.repeat(xTimes);
+                    if (p.charAt(x) === "#") {
+                        t++;
+                    }
+                }
+            })
+            trees === 0 ? trees = t : trees *= t;
+        } else if (checkY[index] === 2) {
+            let t = 0;
+            let y = -2;
+            pattern.forEach((elem) => {
+                x += c;
+                y++;
+                if (y % 2 !== 0 || y <= 0) { //skips every 2 lines
+                    y === 0 ? null : x--;
+                    return;
+                } else {
+                    let p = elem.repeat(xTimes);
+                    if (p.charAt(x) === "#") {
+                        t++;
+                    }
+                }
+
+            })
+            trees === 0 ? trees = t : trees *= t;
+        }
+    });
     return trees;
 }
 
