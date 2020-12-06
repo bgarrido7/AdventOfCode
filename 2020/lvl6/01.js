a1264197db2e81dfb744b64d52de9fa75dfd936e
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-06 15:55:36 +0000

    day 6 part 1 refactored

--- 2020/lvl6/01.js
+++ 2020/lvl6/01.js
@@ -1,26 +1,20 @@
-//const questions = require('fs').readFileSync('input.txt').toString().split('\n');
-const questions = require('fs').readFileSync('example.txt').toString().split('\n');
+const questions = require('fs').readFileSync('input.txt').toString().split('\n');
+//const questions = require('fs').readFileSync('example.txt').toString().split('\n');
 
 function doThing() {
     let count = 0
     let answers = [];
 
     questions.forEach((letters, index, vector) => {
-        if (letters === "") {
+        for (let l of letters) {
+            answers.includes(l) ? null : answers.push(l);
+        }
+        if (letters === "" || vector[index + 1] === undefined) {
             count += answers.length
             answers = [];
-        } else if (vector[index + 1] === undefined) {
-            for (let l of letters) {
-                answers.includes(l) ? null : answers.push(l);
-            }
-            count += answers.length
-        } else {
-            for (let l of letters) {
-                answers.includes(l) ? null : answers.push(l);
-            }
         }
+
     });
     return count;
 }
-
 console.log(doThing());
