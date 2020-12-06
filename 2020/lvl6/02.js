f98a59c4120cd8845cddd06b21fe5a9a09bd204e
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-06 17:11:26 +0000

    trying...

--- 2020/lvl6/02.js
+++ 2020/lvl6/02.js
@@ -1,19 +1,36 @@
-const questions = require('fs').readFileSync('input.txt').toString().split('\n');
-//const questions = require('fs').readFileSync('example.txt').toString().split('\n');
+//const questions = require('fs').readFileSync('input.txt').toString().split('\n');
+const questions = require('fs').readFileSync('example.txt').toString().split('\n');
 
 function doThing() {
     let count = 0
-    let answers = [];
-
+    let group = [];
     questions.forEach((letters, index, vector) => {
-        for (let l of letters) {
-            answers.includes(l) ? null : answers.push(l);
-        }
-        if (letters === "" || vector[index + 1] === undefined) {
-            count += answers.length
-            answers = [];
+        let answers = [];
+        if (letters !== "") {
+            for (let l of letters) { answers.push(l); }
+            group.push(answers);
         }
 
+        if (letters === "" || vector[index + 1] === undefined) {
+            console.log(group)
+
+            if (group.length == 1) {
+                count += group[0].length;
+            } else {
+
+                // let yes = group[0];
+                // group.shift();
+                // yes.forEach(letter => {
+                //     let add = false;
+                //     for (let g of group) {
+                //         (g.indexOf(letter) >= 0) ? add = true: null;
+                //     }
+                //     add ? count++ : null;
+                // });
+            }
+            answers = [];
+            group = []
+        }
     });
     return count;
 }
