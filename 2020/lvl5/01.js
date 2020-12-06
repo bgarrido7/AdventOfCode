a7d5ef3e805039a34e0bd6745f3b5a85afec0df6
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-06 12:44:21 +0000

    fds que bonito

--- 2020/lvl5/01.js
+++ 2020/lvl5/01.js
@@ -1,58 +1,25 @@
 const plane = require('fs').readFileSync('input.txt').toString().split('\n');
 //const plane = require('fs').readFileSync('example.txt').toString().split('\n');
 
-function binarySort(lower, upper, move) {
-    switch (move) {
-        case "F":
-        case "L":
-            return Math.trunc(lower + (upper - lower) / 2)
-        case "R":
-        case "B":
-            return Math.trunc((upper - lower) / 2);
-    }
-}
-
 function doThing() {
-    let highest = 0,
-        id;
+    let row,
+        column;
+    let seats = [];
 
     plane.forEach(elem => {
-        let upper = 128,
-            lower = 1,
-            left = 1,
-            right = 8,
-            row = 0,
-            column = 0;
-        for (let pos in elem) {
-            if (pos > 6 && pos < elem.length - 1) {
-                switch (elem[pos]) {
-                    case "L":
-                        right = binarySort(left, right, "L");
-                        break;
-                    case "R":
-                        left += binarySort(left, right, "R");
-                        break;
-                }
-            } else if (pos == 6) {
-                (elem[pos] == 'F') ? (row = lower) : (row = upper - 1);
-            } else if (pos == elem.length - 1) {
-                (elem[pos] == 'L') ? (column = left) : (column = right - 1);
-            } else {
-                switch (elem[pos]) {
-                    case "F":
-                        upper = binarySort(lower, upper, "F")
-                        break;
-                    case "B":
-                        lower += binarySort(lower, upper, "B")
-                        break;
-                }
-            }
-        }
-        id = (row * 8 + column);
-        id > highest ? highest = id : null;
-        console.log("row:", row, "column:", column, "id:", id)
+        row = elem.slice(0, 7)
+        column = elem.slice(7);
+
+        row = row.replace(/F/gi, 0);
+        row = row.replace(/B/gi, 1);
+        column = column.replace(/R/gi, 1);
+        column = column.replace(/L/gi, 0);
+
+        let id = parseInt(row, 2) * 8 + parseInt(column, 2);
+        seats.push(id);
     });
-    return id;
+
+    return Math.max.apply(null, seats);
 }
 
 console.log(doThing());
