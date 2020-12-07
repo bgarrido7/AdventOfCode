c963f07a3dca757bb915c4316aac104b3bd5bb99
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-07 12:06:15 +0000

    já dáááaááááá

--- 2020/lvl6/02.js
+++ 2020/lvl6/02.js
@@ -1,30 +1,30 @@
-//const questions = require('fs').readFileSync('input.txt').toString().split('\n');
-const questions = require('fs').readFileSync('example.txt').toString().split('\n');
+//const questions = require('fs').readFileSync('example.txt').toString().split('\n');
+const questions = require('fs').readFileSync('input.txt').toString().split('\n');
 
 function doThing() {
     let count = 0;
-    let intersection = new Set()
     let group = [];
     questions.forEach((letters, index, vector) => {
+
         let answers = [];
         if (letters !== "") { //join groups
             for (let l of letters) { answers.push(l); }
             group.push(answers);
         }
         if (letters === "" || vector[index + 1] === undefined) { //end of group
-            if (group.length == 1) count += group[0].length;
-            else {
-                let firstSet = new Set(group.shift());
-                group.forEach(g => {
-                    let newSet = new Set(g);
-                    intersection = new Set([...firstSet].filter(x => newSet.has(x)))
-                });
-                count += intersection.size;
-            }
+            let firstSet = new Set(group[0]);
+
+            let intersection = new Set([...firstSet].filter(x => firstSet.has(x)));
+            group.forEach(g => {
+                nextSet = new Set(g);
+                intersection = new Set([...intersection].filter(x => nextSet.has(x)));
+            });
+            count += intersection.size;
+
             answers = [];
             group = [];
         }
     });
-    return (count);
+    return count;
 }
 console.log(doThing());
