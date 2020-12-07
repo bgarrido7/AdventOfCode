7ba821d080d69218e1b8bbcaea81eb96389a84c1
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-07 19:21:50 +0000

    getting there...

--- 2020/lvl7/01.js
+++ 2020/lvl7/01.js
@@ -2,20 +2,28 @@
 const rules = require('fs').readFileSync('example.txt').toString().split('\n');
 
 function doThing() {
-    let bags = 0;
+    let count = 0;
     familyTree = new Map();
     rules.forEach(r => {
         let cleanRules = r.replace(/bags|bag|no other|(\d)/gi, '').replace(" ,", ",").replace("contain", ",").replace(/\s\s+/g, '').slice(0, -2);
-        let group = cleanRules.split(","); //group of children bags
+        let group = cleanRules.split(","); //group of bags
         let firstBag = group.shift()
         for (let bag of group) {
             familyTree.has(firstBag) ?
                 familyTree.set(firstBag, [familyTree.get(firstBag), bag]) :
                 familyTree.set(firstBag, bag);
         }
-
     });
-    console.log(familyTree);
-    return bags
+    console.log(familyTree)
+    let parentsWithGold = [];
+    familyTree.forEach((value, key, map) => {
+        if (value.indexOf('shiny gold') > -1) {
+            parentsWithGold.push(key)
+            count++;
+        }
+    });
+    console.log(parentsWithGold);
+
+    return count
 }
 console.log(doThing());
