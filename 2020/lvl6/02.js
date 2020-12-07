c1d5a39ce0dc877ecc7a797011a9122340fc655f
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-07 11:33:03 +0000

    pqq nao dá bem fds

--- 2020/lvl6/02.js
+++ 2020/lvl6/02.js
@@ -2,32 +2,29 @@
 const questions = require('fs').readFileSync('example.txt').toString().split('\n');
 
 function doThing() {
-    let count = 0
+    let count = 0;
+    let intersection = new Set()
     let group = [];
     questions.forEach((letters, index, vector) => {
         let answers = [];
-        if (letters !== "") {
+        if (letters !== "") { //join groups
             for (let l of letters) { answers.push(l); }
             group.push(answers);
         }
-        if (letters === "" || vector[index + 1] === undefined) {
-            console.log(group, count)
-            if (group.length == 1) {
-                count += group[0].length;
-            } else {
-                // let yes = group.shift();
-                // let add = [];
-                // yes.forEach(letter => {
-                //     for (let g of group) {
-                //         (g.indexOf(letter) >= 0) ? (add.includes(letter) ? null : add.push(letter)) : null;
-                //     }
-                // });
-                // count += add.length;
+        if (letters === "" || vector[index + 1] === undefined) { //end of group
+            if (group.length == 1) count += group[0].length;
+            else {
+                let firstSet = new Set(group.shift());
+                group.forEach(g => {
+                    let newSet = new Set(g);
+                    intersection = new Set([...firstSet].filter(x => newSet.has(x)))
+                });
+                count += intersection.size;
             }
             answers = [];
-            group = []
+            group = [];
         }
     });
-    return count;
+    return (count);
 }
 console.log(doThing());
