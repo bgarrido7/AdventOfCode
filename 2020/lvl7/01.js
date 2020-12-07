5895a1b1e74b65acd3507f45b9f337f97c5955f8
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-07 20:25:02 +0000

    i give up

--- 2020/lvl7/01.js
+++ 2020/lvl7/01.js
@@ -1,8 +1,19 @@
-//const rules = require('fs').readFileSync('input.txt').toString().split('\n');
-const rules = require('fs').readFileSync('example.txt').toString().split('\n');
+const rules = require('fs').readFileSync('input.txt').toString().split('\n');
+//const rules = require('fs').readFileSync('example.txt').toString().split('\n');
+
+function findParents(parentsWithGold) {
+    let newParents = [];
+    parentsWithGold.forEach(c => {
+        familyTree.forEach((value, key, map) => {
+            if (value.indexOf(c) > -1) {
+                !(newParents.includes(key)) ? newParents.push(key): null;
+            }
+        });
+    });
+    return newParents;
+}
 
 function doThing() {
-    let count = 0;
     familyTree = new Map();
     rules.forEach(r => {
         let cleanRules = r.replace(/bags|bag|no other|(\d)/gi, '').replace(" ,", ",").replace("contain", ",").replace(/\s\s+/g, '').slice(0, -2);
@@ -14,16 +25,22 @@ function doThing() {
                 familyTree.set(firstBag, bag);
         }
     });
-    console.log(familyTree)
-    let parentsWithGold = [];
-    familyTree.forEach((value, key, map) => {
-        if (value.indexOf('shiny gold') > -1) {
-            parentsWithGold.push(key)
-            count++;
-        }
-    });
-    console.log(parentsWithGold);
+    let finalParents = [];
+    let findThisColor = ['shiny gold'];
+    let nextParent;
+    while (true) {
+        nextParent = findParents(findThisColor);
+        if (nextParent == "")
+            break;
+        finalParents.push(nextParent);
+        findThisColor = nextParent;
+    }
 
-    return count
+    //equivalente a um flatmap mas para objects
+    let result = Object.keys(finalParents).reduce(function(r, k) {
+        return r.concat(finalParents[k]);
+    }, []);
+    console.log(result)
+    return result.length;
 }
 console.log(doThing());
