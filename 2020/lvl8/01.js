b8e05c3665195687ea03ed3d1eee1776b68f7d8f
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-08 11:30:36 +0000

    day 8 real easy boy

---
+++ 2020/lvl8/01.js
@@ -0,0 +1,31 @@
+const line = require('fs').readFileSync('input.txt').toString().split('\n');
+//const line = require('fs').readFileSync('example.txt').toString().split('\n');
+
+function doThing() {
+    let acc = 0;
+    let visitedInst = [];
+
+    for (let i = 0; i < line.length; i++) {
+        if (visitedInst.includes(i))
+            break;
+        else
+            visitedInst.push(i);
+
+        // console.log(i, line[i]);
+        let op = line[i].split(" ")[0];
+        let arg = parseInt(line[i].split(" ")[1]);
+        switch (op) {
+            case ('acc'):
+                acc += arg;
+                break;
+            case ('jmp'):
+                i += (arg - 1)
+                break;
+            case ('nop'):
+                break;
+        }
+    }
+    return acc;
+}
+
+console.log(doThing());
