76064e5752356b891d23f2334d70b9b6d6297b55
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-10 12:00:39 +0000

    organized things

--- 2020/lvl_10/02.js
+++
@@ -1,8 +0,0 @@
-//const list = require('fs').readFileSync('input.txt').toString().split('\n');
-const list = require('fs').readFileSync('example.txt').toString().split('\n');
-
-function doThing() {
-   
-}
-
-console.log(doThing())
