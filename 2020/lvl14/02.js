b58cbb52a389217a36a63e379b69a8791e27b0e3
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-14 17:55:32 +0000

    sou uma toninha

--- 2020/lvl14/02.js
+++ 2020/lvl14/02.js
@@ -11,18 +11,29 @@ function doThing() {
     initialization.forEach(ini => {
         if (ini.split(" ")[0] === "mask") {
             mask = ini.split("= ")[1];
+            //console.log(mask)
         } else {
             let mem = parseInt(ini.split("[")[1].split("]")[0]);
-            let val = parseInt(ini.split("= ")[1]).toString(2).padStart(mask.length, "0");
+            let val = parseInt(ini.split("= ")[1]).toString(2);
+            while (val.length - 1 < mask.length - 1) {
+                val = "0" + val;
+            }
+            //console.log("\nval :", val);
 
             mask.split("").forEach((bit, index) => {
                 if (bit !== "X") {
                     val = replaceAt(val, index, bit);
+                    //console.log(index, bit, val.charAt(index))
                 }
             });
+            //console.log("mask:", mask, "\nnewV:", val);
+
             res[mem] = parseInt(parseInt(val, 2).toString(10));
         }
+
     });
+    //console.log(res)
+
     return res.reduce((a, b) => a + b);
 }
 
