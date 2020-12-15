0e3c863e6e2fd905622c015e6f36f95651fd195c
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-15 13:13:58 +0000

    apto para multiplos testes

--- 2020/lvl15/01.js
+++ 2020/lvl15/01.js
@@ -1,24 +1,34 @@
 const numbers = [2, 20, 0, 4, 1, 17];
-//const numbers = [3, 1, 2];
 
-function doThing() {
-    let myNums = numbers.slice(0);
-    for (let i = numbers.length + 1; i < 2018 + numbers.length; i++) {
-
+function doThing(nums, turn) {
+    let myNums = nums.slice(0);
+    let i
+    for (i = nums.length + 1; i < turn + nums.length; i++) {
         let n = myNums[myNums.length - 1];
 
         if (myNums.indexOf(n) == myNums.length - 1)
             myNums.push(0);
         else {
             let tmpNum = myNums.slice(0, -1).lastIndexOf(n) + 1;
-            //console.log(myNums.lastIndexOf(n) + 1, tmpNum)
             n = myNums.lastIndexOf(n) + 1 - tmpNum;
             myNums.push(n);
         }
     }
-    console.log(myNums.slice(-3))
+    //console.log(i)
+    //console.log(myNums)
     return myNums[myNums.length - 1];
 }
 
+let testArray = [
+    [0, 3, 6],
+    [1, 3, 2],
+    [2, 1, 3],
+    [1, 2, 3],
+    [2, 3, 1],
+    [3, 2, 1],
+    [3, 1, 2],
+];
 
-console.log(doThing())
+testArray.forEach(t => { console.log(doThing(t, 2018)) })
+
+console.log(doThing(numbers, 2018))
