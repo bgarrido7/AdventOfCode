6648c0738b1afb8e0dece06dacce64e8e5e284f5
bgarrido7 <bianevesgarrido@gmail.com>
2020-12-15 13:28:57 +0000

    day 15 part 1 done

--- 2020/lvl15/01.js
+++ 2020/lvl15/01.js
@@ -1,21 +1,18 @@
 const numbers = [2, 20, 0, 4, 1, 17];
 
-function doThing(nums, turn) {
+function doThing(nums) {
     let myNums = nums.slice(0);
-    let i
-    for (i = nums.length + 1; i < turn + nums.length; i++) {
+    for (let i = nums.length + 1; i <= 2020; i++) {
         let n = myNums[myNums.length - 1];
 
-        if (myNums.indexOf(n) == myNums.length - 1)
-            myNums.push(0);
+        if (myNums.indexOf(n) === myNums.length - 1) myNums.push(0);
+
         else {
             let tmpNum = myNums.slice(0, -1).lastIndexOf(n) + 1;
             n = myNums.lastIndexOf(n) + 1 - tmpNum;
             myNums.push(n);
         }
     }
-    //console.log(i)
-    //console.log(myNums)
     return myNums[myNums.length - 1];
 }
 
@@ -29,6 +26,6 @@ let testArray = [
     [3, 1, 2],
 ];
 
-testArray.forEach(t => { console.log(doThing(t, 2018)) })
+testArray.forEach(t => { console.log(doThing(t, 2020)) })
 
-console.log(doThing(numbers, 2018))
+console.log(doThing(numbers));
