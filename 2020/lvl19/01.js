45e9b122a8f24077f38b3301dd8fc26a74ae37a3
bgarrido7 <bianevesgarrido@gmail.com>
2021-01-01 15:44:39 +0000

    i'm too tired man

--- 2020/lvl19/01.js
+++ 2020/lvl19/01.js
@@ -1,8 +1,20 @@
-//const notes = require('fs').readFileSync('input.txt').toString().split('\n');
-const notes = require('fs').readFileSync('example.txt').toString().split('\n');
+//const input = require('fs').readFileSync('input.txt').toString().split('\n\n');
+const input = require('fs').readFileSync('example.txt').toString().split('\n\n');
 
 function doThing() {
-   
+    let rules = input[0];
+    let msg = input[1];
+
+    let valid = [];
+    rules.split("\n").forEach(r => {
+        console.log(r)
+        r.splice(0, 3);
+        valid.push(r);
+    });
+    msg.split("\n").forEach(m => {
+        console.log(m)
+    });
+
 }
 
 console.log(doThing())
