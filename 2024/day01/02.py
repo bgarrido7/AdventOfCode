de50246a0fc6513771a8b1a177929594958ccc16
bgarrido7 <CTW01207@criticaltechworks.com>
2024-12-09 13:04:41 +0100

    day 1 totally done

--- 2024/day01/02.py
+++ 2024/day01/02.py
@@ -1,8 +1,16 @@
-import re
+left = []
+right = []
 
-f = open("example.txt", 'r')
-# f = open("input.txt", 'r')
-sum = 0
+with open('input.txt', 'r') as file:
+#with open('example.txt', 'r') as file:
+    for line in file:
+        left.append(int(line.split()[0]))
+        right.append(int(line.split()[1]))
 
-print("sum = ",sum)
-
+total = 0
+for i in range(len(right)):
+    num = left[i]
+    times = right.count(num)
+    total += num * times 
+    
+print(total)
