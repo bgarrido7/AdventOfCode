c965930d2e0a0e71d7932e1b9d4efaf7cdbac333
bgarrido7 <CTW01207@criticaltechworks.com>
2024-12-09 16:35:32 +0100

    day 2 pt 1 done

--- 2024/day02/01.py
+++ 2024/day02/01.py
@@ -1,13 +1,21 @@
-levels = []
 safe = 0
 
-#with open('input.txt', 'r') as file:
-with open('example.txt', 'r') as file:
+def isSorted(array) -> bool:
+    return sorted(array) == array or sorted(array, reverse=True) == array
+  
+def isDiffer(array) -> bool:    
+    for i in range(len(array)-1):
+        differ = abs(array[i] - array[i+1]) 
+        if(differ>3 or differ<1):
+            return False
+    return True
+       
+with open('input.txt', 'r') as file:
+#with open('example.txt', 'r') as file:
     for report in file:
-        levels = report.split()
-        levels = list(map(int, levels))
-        print((levels))
-
-
+        levels = list(map(int, report.split()))
     
-#print(safe)
+        if isSorted(levels) and isDiffer(levels):
+           safe+=1
+            
+print(safe)
