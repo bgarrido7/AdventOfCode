8702696f0f4dc3526d0c5f8284fa43fc7138ddb4
bgarrido7 <CTW01207@criticaltechworks.com>
2024-12-09 12:51:13 +0100

    example done

--- 2024/day01/01.py
+++ 2024/day01/01.py
@@ -1,8 +1,17 @@
-import re
+left = []
+right = []
 
-f = open("example.txt", 'r')
-# f = open("input.txt", 'r')
+with open('input.txt', 'r') as file:
+#with open('example.txt', 'r') as file:
+    for line in file:
+        left.append(int(line.split()[0]))
+        right.append(int(line.split()[1]))
 
-for content in f:
-    print(content)
+left.sort()
+right.sort()
 
+total = 0
+for i in range(len(right)):
+    total += right[i] - left[i]
+    
+print(total)
